bafd025a804ab21b802c2cd9e6a80486b0f67574
Danilo Nascimento <dan.silva.13.zx@gmail.com>
2025-03-24 08:41:08 -0300

    New custom packages evil-collection and alchemist-minimal-fork

---
+++ private/local/evil-collection-alchemist-minimal-fork/evil-collection-alchemist-minimal-fork.el
@@ -0,0 +1,59 @@
+;;; evil-collection-alchemist-minimal-fork.el --- Evil bindings for `alchemist-minimal-fork' -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025 Danilo Nascimento
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; Evil bindings for `compile'.
+
+;;; Code:
+(require 'evil-collection)
+(require 'alchemist-minimal-fork)
+
+(defconst evil-collection-compile-maps '(alchemist-test-report-mode-map))
+
+;;;###autoload
+(defun evil-collection-alchemist-minimal-fork-setup ()
+  "Set up `evil' bindings for `alchemist-minimal-fork'."
+  (evil-set-initial-state 'alchemist-test-report-mode-map 'normal)
+  (evil-collection-set-readonly-bindings 'alchemist-test-report-mode-map)
+
+  (dolist (keymap evil-collection-compile-maps)
+
+    (evil-collection-define-key nil keymap
+      "g" nil)
+
+    (evil-collection-define-key 'normal keymap
+      ;; (kbd "RET") 'compile-goto-error
+
+      ;; "go" 'compilation-display-error
+      ;; (kbd "S-<return>") 'compilation-display-error
+
+
+      (kbd "TAB") 'alchemist-test-next-result
+      (kbd "S-TAB") 'alchemist-test-previous-result
+
+      "gf" 'find-file-at-point
+      "t" 'toggle-truncate-lines
+      "gj" 'alchemist-test-next-result
+      "gk" 'alchemist-test-previous-result
+      (kbd "C-j") 'alchemist-test-next-result
+      (kbd "C-k") 'alchemist-test-previous-result
+      "[[" 'alchemist-test-previous-stacktrace-file
+      "]]" 'alchemist-test-next-stacktrace-file
+      "gr" 'alchemist-mix-rerun-last-test)))
+
+(provide 'evil-collection-alchemist-minimal-fork)
+;;; evil-collection-alchemist-minimal-fork.el ends here
